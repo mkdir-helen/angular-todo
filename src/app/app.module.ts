bec5fdefe5190ba35f14e8ae5a49c988814b9fbd
mkdir-helen <experiment.helen@gmail.com>
2019-02-11 17:14:55 -0500

    created login and signup component

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    FontAwesomeModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  invalidLogin = false;
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login(formValues) {
+    console.log('login.component login');
+
+    this.authService.login(formValues.email, formValues.password)
+      .subscribe(result => {
+        if (!result) {
+          console.log('login.component user not found');
+          this.invalidLogin = true;
+        } else {
+          console.log('login.component logged in. redirecting to home page');
+          this.invalidLogin = false;
+          this.router.navigate(['/']);
+        }
+      });
+  }
+
+}
